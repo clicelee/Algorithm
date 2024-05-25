2e88c3b90e273baa9e0e8dcac0f939970a129f7d
cl i c e <131771046+clicelee@users.noreply.github.com>
2024-05-25 12:17:01 +0900

    Update 가장 큰 수 찾기.js

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -8,3 +8,10 @@ const solution=(array)=>{
         }
     return [max,parseInt(max_index)];
 }
+//`for...in` 루프를 사용하면 배열의 인덱스가 문자열로 반환되기 때문에 `i`가 숫자가 아닌 문자로 인식된다
+//그래서 index를 배열로 반환할떄는 parseInt를 사용해야 한다
+/* 더 좋은 풀이 
+//max 함수 사용
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
+*/
