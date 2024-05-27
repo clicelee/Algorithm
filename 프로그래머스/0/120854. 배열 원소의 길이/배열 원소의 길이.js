7064aec808ff6e5c820730e87ac28e922b626234
clice <131771046+clicelee@users.noreply.github.com>
2024-05-27 13:04:43 +0900

    Update 배열 원소의 길이.js

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,3 +1,7 @@
 const solution=(strlist)=>strlist.map(function(value){ return value.length});
 //return array.map(function(i){ return array[i].length});
 //map(function(i))에서 i는 그 자체로 배열의 원소이다
+
+
+// const solution=(strlist)=> strlist.map((el) => el.length);
+// 배열의 원소element를 el로한다
