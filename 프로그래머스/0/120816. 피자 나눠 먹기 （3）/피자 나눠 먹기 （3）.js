25ac4ac305c305455ec7666ef3ba5f2758970981
clice <131771046+clicelee@users.noreply.github.com>
2024-05-29 01:38:38 +0900

    Update 피자 나눠 먹기 （3）.js

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -6,3 +6,7 @@ const solution=(slice, n)=> ~~(n/slice)+((~~n%slice)===0?0:1);
 // if(나머지 있음) 몫+1
 // if(나머지 없음) 몫
 // 몫+나머지===0?0:1
+
+/* 더 좋은 풀이 
+올림 사용 */
+// const solution=(slice, n)=>Math.ceil(n / slice)
