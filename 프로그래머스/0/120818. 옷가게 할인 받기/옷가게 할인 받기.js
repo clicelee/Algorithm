4089e3f1c156faf22e559abdd1e09fa070731852
clice <131771046+clicelee@users.noreply.github.com>
2024-05-28 13:28:26 +0900

    Update 옷가게 할인 받기.js 줄 바꿈 수정
    한줄로 밀어넣기

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,2 +1,1 @@
-const solution=(price) => 
-~~(price*(1-(price>=500000?0.2:(price>=300000?0.1:(price>=100000?0.05:0)))));
+const solution=(price) => ~~(price*(1-(price>=500000?0.2:(price>=300000?0.1:(price>=100000?0.05:0)))));
