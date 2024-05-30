13b6e4480e02a156dd8174c5572d76e9c3cd51b0
clice <131771046+clicelee@users.noreply.github.com>
2024-05-31 02:45:44 +0900

    Update 숨어있는 숫자의 덧셈 （1）.js
    -스프레드 연산자
    -reduce 함수로 sum 구하기

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -13,3 +13,9 @@ const isNumber=(value)=>{
 
 //이전 문제: 프로그래머스/0/120893. 대문자와 소문자 에서 배운 for of 문을 사용해보았다
 //for of 문에서 원소는 '문자'로 취급되기 때문에, 숫자로서 검증을 할 때는 Number()을 사용해야 한다.<< ⭐️ 이 부분에서 시간 많이 썼다
+
+
+//다시 생각을 해보니     
+//return [...my_string].reduce((acc,cur)=>Number(cur) ? +acc + +cur : acc, 0) 
+//이 코드가 더 좋은것 같다
+//스프레드 연산자, reduce 함수 체크
