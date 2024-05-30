af3a7eef5f8855bc0804a4a89f4fe94e848e714e
clice <131771046+clicelee@users.noreply.github.com>
2024-05-30 10:11:53 +0900

    Update 특정 문자 제거하기.js

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,1 +1,4 @@
 const solution=(my_string, letter)=>[...my_string].filter((value,index,arr)=> value!=letter).join('');
+//더 좋은 풀이: my_string.split(letter).join('')
+//string 을 바로 split할 수 있다
+//또한 두번쨰 방법이 더 빠르다
