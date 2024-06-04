f6dbf4bd728cf4e9b45e50c22b2c310aa1821f70
clice <131771046+clicelee@users.noreply.github.com>
2024-06-04 23:29:39 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,7 @@
+//중복된 값을 허용하지 않는 Set 객체를 쓰면 좋겠다
+const solution=(string)=>{
+    var answer = new Set();
+    for(i of string) answer.add(i);
+    return [...answer].join('');
+}
+//Set은 객체이기 때문에 배열 메소드를 쓸 때는 스프레드 연산자로 (객체->배열) 변환을 해준다
