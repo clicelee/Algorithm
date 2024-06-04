d6630d56ff97cd1baefba2c68a9bea0ba9f5580f
clice <131771046+clicelee@users.noreply.github.com>
2024-06-04 23:31:19 +0900

    Set 객체 능숙하게 사용하기

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -5,3 +5,7 @@ const solution=(string)=>{
     return [...answer].join('');
 }
 //Set은 객체이기 때문에 배열 메소드를 쓸 때는 스프레드 연산자로 (객체->배열) 변환을 해준다
+
+/* better solution */
+//바로 Set 객체 사용
+//return [...new Set(my_string)].join('');
