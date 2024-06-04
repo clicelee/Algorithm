fb9b9f5813e68ecc47fa82c6273b99603a7d6faa
clice <131771046+clicelee@users.noreply.github.com>
2024-06-04 23:18:01 +0900

    정규표현식, 숫자->문자열 변환해야 replace()사용가능

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,1 +1,8 @@
+//정규표현식!
 const solution=(order)=> order.toString().replace(/[^369]/g, '').length;
+
+//틀려서 고생한 풀이
+//replace()은 배열이나 문자열에서 사용가능하다
+//order은 숫자이기 때문에 꼭 toString()을 사용하여 문자열로 바꿔줘야 한다
+// order.replace(/[^369]/g,'');
+// return order.length;
