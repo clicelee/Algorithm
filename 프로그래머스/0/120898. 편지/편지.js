372686a62909b5a33801eda9ab31a656f6d440bb
clice <131771046+clicelee@users.noreply.github.com>
2024-06-04 21:12:04 +0900

    문자열은 배열이기 때문에 message.length

--- 프로그래머스/0/120898. 편지/편지.js
+++ 프로그래머스/0/120898. 편지/편지.js
@@ -1,3 +1,4 @@
+//사실 가장 쉬운 방법은 return 2*(message.length); 이다
 const solution=(message)=>{
     // return 2*(Array.from(message)).length;
     // return 2*(message.split('').length);
@@ -5,3 +6,4 @@ const solution=(message)=>{
 }
 // 문자열을 배열로 바꾸는 방법에는 3가지가 있다
 // 그 중 [...스프레드 연산자]가 제일 빠르다
+
