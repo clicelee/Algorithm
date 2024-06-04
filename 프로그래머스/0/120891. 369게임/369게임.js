0df0bd1b51bcc322a79d262d5f20d9f8382ad0b7
clice <131771046+clicelee@users.noreply.github.com>
2024-06-04 23:19:15 +0900

    matchAll()

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -6,3 +6,8 @@ const solution=(order)=> order.toString().replace(/[^369]/g, '').length;
 //order은 숫자이기 때문에 꼭 toString()을 사용하여 문자열로 바꿔줘야 한다
 // order.replace(/[^369]/g,'');
 // return order.length;
+
+/* better solution */
+//matchAll 사용
+//얘 또한 배열, 문자열에서 사용 가능하기에 toString 써주기
+//answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
