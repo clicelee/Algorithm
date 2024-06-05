67941c0ef2711f15c5c0964da0e8460b7a03b1a5
clice <131771046+clicelee@users.noreply.github.com>
2024-06-06 03:02:37 +0900

    정수인지 판단하는 Number.isInteger()

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -2,3 +2,6 @@ const solution=(n)=>{
     let newN=Math.sqrt(n); //제곱근을 구한다
     return (newN===~~newN)?1:2; //제곱근에 소수점이 없다면, 올바른 제곱수이다
 }
+/* better solution */
+//isInteger 사용
+//Number.isInteger(Math.sqrt(n)) ? 1 : 2;
