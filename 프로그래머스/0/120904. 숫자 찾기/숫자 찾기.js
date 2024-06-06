a35d1417d93106aa5c110517272da54a786642f4
clice <131771046+clicelee@users.noreply.github.com>
2024-06-07 03:03:57 +0900

    for in 문에서 i는 문자

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,8 +1,23 @@
 const solution=(num,k)=>{
     let array=[...num.toString()];
+    // console.log("array:",array);
+    // console.log("i 값 k");
     for(i in array){
-        if(array[i]==k) 
-            return Number(i)+1
-    };
-    return -1;
+        // console.log(i,array[i],k);
+        if(array[i]==k){
+            return Number(i)+1; //중요, for in 문에서 i는 문자열이기 때문에 연산을 하려면 Number 함수로 숫자변환을 해줘야 한다
+        }
+    }
+       return -1;
 }
+/* 기존 코드 */
+// function solution(num, k) {
+//     let array=[...num.toString()];
+//     for(let i of array){
+//         console.log(i);
+//         // if(i===k) console.log(i+1);
+//         // else
+//             return -1;   
+//     }
+// }
+
