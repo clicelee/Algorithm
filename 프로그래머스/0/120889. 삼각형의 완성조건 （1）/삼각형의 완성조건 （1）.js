3e4b722afa18c86b653573caaa3cbc3f1b4ebf1b
clice <131771046+clicelee@users.noreply.github.com>
2024-06-07 13:18:02 +0900

    Update 삼각형의 완성조건 （1）.js

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,5 +1,5 @@
 function solution(sides) {
     sides.sort((a,b)=>a-b);
-    console.log(sides);
+    // console.log(sides); 이걸 출력하면 시간을 4ms나 더 많이 잡아먹는다 
     return ((sides[0]+sides[1])>sides[2])?1:2;
 }
