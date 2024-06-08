29c1ffe49288d48e09091699fee8bde044e780ef
clice <131771046+clicelee@users.noreply.github.com>
2024-06-09 00:23:01 +0900

    마지막 원소를 가져오는 .pop() 메서드

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,4 +1,5 @@
+
 const solution=(n)=>{
     if(n[1]/n[0]===n[2]/n[1]) return n[n.length-1]*(n[1]/n[0]);
-    else return n[n.length-1]+(n[1]-n[0]);
+    else return n[n.length-1]+(n[1]-n[0]); //여기서 마지막 원소를 가져올 때 n.pop()를 이용하면 더 편하다
 }
