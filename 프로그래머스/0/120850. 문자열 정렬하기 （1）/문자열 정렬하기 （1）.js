f0005f33e7cd9c21572f7f0caf252003f2fc214c
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-12 03:47:23 +0900

    문자열에서 숫자만, 그리고 배열로 변환

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,1 +1,13 @@
 const solution=(my_string)=> my_string.split('').map(Number).filter(n=>(isNaN(n)==false)).sort((a,b)=>a-b);
+
+/* 한줄로 만들기 위한 시행착오들
+function solution(my_string) {
+    // let changeToNumber = my_string.match(/\d+/g) //12392
+    let changeToArray=[...my_string]; //h i 1 2 3 9 2
+    
+    
+    (changeToArray.map(Number)).sort((a,b)=>a-b); 
+    return changeToArray.join('').split('');
+}
+
+*/
