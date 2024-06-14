64c9d1e5aa2254c914fc17a6f4d1a2b64aed3e7c
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-14 11:33:10 +0900

    Math.abs()

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,1 +1,3 @@
 const solution=(dots)=>Math.abs((dots.sort((a,b)=>a[0]-b[0])[3][1]-dots.sort((a,b)=>a[0]-b[0])[2][1])*(dots.sort((a,b)=>a[0]-b[0])[2][0]-dots.sort((a,b)=>a[0]-b[0])[0][0]))
+
+//x좌표를 기준으로 정렬하였다, 그리고 두개씩 묶어 x좌표 y좌표 차이를 구한 다음 곱하고, 그 값에 절댓값 Math.abs 사용
