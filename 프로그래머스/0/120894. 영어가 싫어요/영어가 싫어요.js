91ce2a8522980e202626c718578ee9284ccc68b7
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-15 18:01:30 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,11 @@
+const englishAndNumber={
+    zero:0, one:1, two:2, three:3, four:4, five:5,
+    six:6, seven:7, eight:8, nine:9
+}
+
+const solution=(numbers)=>{
+    arrayedNumber=numbers.split(/(one|two|three|four|five|six|seven|eight|nine)/).filter(Boolean);
+    let answer=[];
+    for(let i of arrayedNumber) answer.push(englishAndNumber[i]);
+    return parseInt(answer.join(''));
+}
