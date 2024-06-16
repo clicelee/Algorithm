e8880cdcfb34a33aa62313752c8e681fc885aac7
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-16 12:38:02 +0900

    Reduce를 이용해 답을 더해나가기

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -12,3 +12,11 @@ const solution=(letter)=>{
     for(let i of arrayedLetter) answer.push(morse[i]);
     return answer.join('');
 }
+
+/* better solution */
+// map이나 push 를 이용하는 것 보다, reduce로 답에 계속 더해나가는 방식이 더 편리하다
+/*
+function solution(letter) {
+    return letter.split(' ').reduce((prev, curr) => prev + morse[curr], '')
+}
+*/
