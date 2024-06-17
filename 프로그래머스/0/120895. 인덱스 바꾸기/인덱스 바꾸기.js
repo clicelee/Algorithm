e60c9d9fa6ff30efdbfe9435cc7f7f6b45b66e15
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-17 22:12:22 +0900

    즉시 실행함수 IIFE

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -4,3 +4,6 @@ const solution=(my_string, num1, num2)=>{
     [array[num1], array[num2]] =[array[num2], array[num1]];
     return array.join('');
 }
+/*Better solution */
+//즉시 실행함수 IIFE를 활용하면 한줄로도 줄일 수 있다
+// const solution = (my_string, num1, num2) => (s => ([s[num1], s[num2]] = [s[num2], s[num1]], s.join('')))([...my_string]);
