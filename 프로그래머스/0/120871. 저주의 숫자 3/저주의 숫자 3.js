18090011900baf0785c8d879487ef94a62ced9af
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-23 23:01:55 +0900

    시행착오, other solution 추가

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -8,3 +8,40 @@ const solution=(n)=>{
     }
     return number;
 }
+
+/* 시행착오 */
+//1직전까지 3의 배수의 개수를 구하고 더한다
+//2직전까지 3이 들어간 수를 구하고 더한다
+//3직전까지 3의 배수이자 3이 들어간 수를 뺀다(1,2에서 겹치니까)
+// function createIntegerArray(n){
+//     let array=[];
+//     let 개수=0;
+//     for (let i=1; i <= n; i++){
+//         if(i%3===0) { //3의 배수인지
+//             let count=0;
+//             개수++;
+//             array.push(i);
+//         }
+//         if(n.split('').hasOwnProperty(3)){ //3이 들어가는지
+//             array.push(i);
+//             count++;
+//         }
+//         if(count==1)
+//     }
+    
+//     return array;
+// }
+    
+// const solution=(n)=>{
+//     console.log("3의 배수의 개수: ", ~~(n/3));
+//     createIntegerArray(n);
+// }
+
+
+
+/* other solution */
+function solution(n) {
+  return [...Array(n * 3)]
+    .map((_, i) => i + 1)
+    .filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
+}
