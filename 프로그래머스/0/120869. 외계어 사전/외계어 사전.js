607c8e28ddb3a5ef4cc2fda55cce45a152e62772
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-25 23:58:05 +0900

    [level 0] Title: 외계어 사전, Time: 1.98 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,20 +1,11 @@
-// function solution(spell, dic) {
-//     var answer = 0;
-//     for(i of dic){
-//         console.log(i);
-//         console.log(spell.includes(i.toString()));
-//     }
-//         return answer;
-// }
-const solution=(spell, dic)=>{
-    var sortedSpell=spell.slice().sort().join('');
-    for (let word of dic){
-        var sortedWord=word.split('').sort().join('');
-        if (sortedSpell===sortedWord){
-            console.log(word);
-            return 1;
-        }
+function solution(spell, dic) {
+    var answer = 0;
+    for(i of dic){
+        console.log(i);
+        console.log(spell.include(i.toString()));
     }
-    return 2;
+        return answer;
 }
+
+//dic을 쪼개고 하나씩 검사를 한다
 
