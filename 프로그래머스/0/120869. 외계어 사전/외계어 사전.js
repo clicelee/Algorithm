8646100b692caae3e0f258e7e0a153f897e6487b
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-26 08:56:12 +0900

    제출풀이 오류 수정
    extension이 다른 풀이를 기록하여 수정함

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,11 +1,26 @@
-function solution(spell, dic) {
-    var answer = 0;
-    for(i of dic){
-        console.log(i);
-        console.log(spell.include(i.toString()));
+/* 제출 풀이*/
+const solution=(spell, dic)=>{
+    var sortedSpell=spell.slice().sort().join('');
+    for (let word of dic){
+        var sortedWord=word.split('').sort().join('');
+        if (sortedSpell===sortedWord){
+            console.log(word);
+            return 1;
+        }
     }
-        return answer;
+    return 2;
 }
+
+
+
+// function solution(spell, dic) {
+//     var answer = 0;
+//     for(i of dic){
+//         console.log(i);
+//         console.log(spell.include(i.toString()));
+//     }
+//         return answer;
+// }
 
 //dic을 쪼개고 하나씩 검사를 한다
 
