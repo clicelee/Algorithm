f10349cb68b68740d4960fef3ee0b2554bab75f4
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-29 22:01:29 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -1,13 +1,3 @@
-const solution=(num, total)=>{
-    let middle=total/num; //total을 num으로 나눔
-    if(middle%1!=0) middle-=0.5; //만약 num이 짝수라서 middle 값이 소수인 경우, 0.5를 빼줌
-    let array=[middle];
-    for(i=1; i<=~~(num/2); i++){
-        array.push(middle-i);
-        array.push(middle+i);
-    }
-    array.sort((a,b)=>a-b);
-    if(num%2===0) array.shift();
-    return array;
+function solution(num, total) {
+    return [total/num-2, total/num-1];
 }
-
