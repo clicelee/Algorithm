fb99ae649a00073ad96905769552e096afec0462
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-06-30 17:59:41 +0900

    add: better solution
    연속된 수의 합=>등차수열의 합 공식 이용

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.js
@@ -10,3 +10,16 @@ const solution=(num, total)=>{
     if(num%2===0) array.shift();
     return array;
 }
+
+
+/*better solution */
+// m=num×n + (num−1)×num/2 이고
+// n=2×total−num×(num−1) / 2×num 이다
+
+
+// function solution(num, total) {
+//     const a = (2 * total / num + 1 - num) / 2
+//     return Array(num).fill().map((_, i) => i + a)
+// }
+
+
