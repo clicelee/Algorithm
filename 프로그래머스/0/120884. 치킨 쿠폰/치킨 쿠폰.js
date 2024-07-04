c558e027338fe8ab38205444051e8a11c2fb1da7
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-04 16:44:54 +0900

    무한루프 실수, 잘못된 값 누적하여 더함

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -7,3 +7,11 @@ const solution = (chicken) => {
     }
     return serviceChicken;
 }
+
+
+// 내가 놓친 실수
+// while(coupon>=10){
+//     serviceChicken+=~~(coupon/10);
+//     coupon=coupon%10+serviceChicken;
+// }
+//여기서 coupon에 serviceChicken을 누적하여 더하니까 while문을 돌수록 누적되어 무한루프에 걸렸다
