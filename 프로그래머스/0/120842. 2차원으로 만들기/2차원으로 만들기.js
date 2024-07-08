b16ded3804bf08c970cd02843a3dc9ac8a046bb8
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-08 16:38:33 +0900

    Update 2차원으로 만들기.js

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,5 +1,14 @@
+/* map 함수로 더 개선된 코드*/
 const solution=(num_list, n)=>{
     return Array(num_list.length / n).fill().map((_, i) =>
         num_list.slice(i*n, (i+1)*n)
     );
 }
+
+/* 처음 작성했던 코드 */
+// function solution(num_list, n) {
+//     let result = [];
+//     for(let i = 0; i < num_list.length/n; i++)
+//         result[i]=num_list.slice(i*n,i*n+n);
+//     return result;
+// }
