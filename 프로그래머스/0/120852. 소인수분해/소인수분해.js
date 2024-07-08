bea1b220d9fb7d2b9c8f6676078d0a8324bc9c1f
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-08 18:17:25 +0900

    better solution for finding prime numbers

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -9,3 +9,30 @@ const solution = (n) => {
             }
     return answer;
 }
+
+/* better solution */
+
+// function solution(n) {
+//     var list = [];
+
+//     function isPrime(n){
+//         if (n == 2)
+//             return true;
+
+//         for (let i = 2; i < n; i++){
+//             if (n%i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     for (let i = 2; i <= n; i++){
+//         if ((isPrime(i))&&(n%i == 0)){
+//             list.push(i);
+//             n = n/i;
+//         }
+//     }
+
+//     return list;
+// }
