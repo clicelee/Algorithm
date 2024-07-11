4dcce4d247f60fe6293f478192e1eff2cac324ff
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-11 17:50:42 +0900

    특정 조건을 만족하는 value만 뽑아 새로운 array를 만드는 method: filter

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,2 +1,6 @@
 const solution=(cipher, code) => [...cipher].filter((number,index)=>(index + 1) % code === 0).join('')
+//몇주전에는 이렇게 for 문으로 삽질하다가 못 풀었는데, 오늘 문제를 다시 보고 filter가 바로 생각났다
 
+// for(i=0;i+code*1-1<cipher.length; i++) 
+//     answer[i]=(cipher[i+code*i-1]);
+// return answer;
