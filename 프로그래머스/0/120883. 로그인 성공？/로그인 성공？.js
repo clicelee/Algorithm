951f1cf623f0530290ea58117e4f53bcbdcfc8a6
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-12 17:02:35 +0900

    Map의 has get 메서드, some, 구조분해할당
    https://clice.tistory.com/entry/JS-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%84%B1%EA%B3%B5-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-120883

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,7 +1,22 @@
-function solution(id_pw, db) {
+const solution = (id_pw, db) => {
     let id=id_pw[0];
     let pw=id_pw[1];
     for(i of db)
         if(i[0]===id) return (i[1]===pw)?("login"):("wrong pw");
     return "fail"
 }
+/* better solution */
+
+//map을 이용하였다
+//has: 값이 있는가?
+//get: 값을 가져온다
+function solution(id_pw, db) {
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+}
+
+
+/* best solution */
+//구조분해 할당, some 메서드
+var solution=([a,b],d)=>d.some(([p,q])=>a==p&&b==q)?'login':d.some(([p])=>a==p)?'wrong pw':'fail'
