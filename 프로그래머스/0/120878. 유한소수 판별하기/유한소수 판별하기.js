b122c844bb8d44d2b75fe3f0b7398ce7e32b9b80
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-15 15:28:48 +0900

    add better solution: using toFixed() method

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,3 +1,4 @@
+/* my solution */
 const solution=(a, b) => {
     let minimizedB=b/GCM(a,b);
     while (minimizedB % 2 <= 0) minimizedB /= 2;
@@ -23,3 +24,26 @@ const getAliquot = (num) => {
     }
     return aliquotNum;
 }
+
+/*better solution */
+function solution(a, b) {
+    let n = 1;
+    for (let i = 1; i <= Math.min(a,b); i++) {
+        if (a%i===0 && b%i===0) n = i;
+    }
+    b/=n;
+    while (b%2===0) b/=2;
+    while (b%5===0) b/=5;
+
+    return b === 1 ? 1 : 2;   
+}
+
+
+/* other solution */
+
+function solution(a, b) => Number((a/b).toFixed(10)) == a/b ? 1 : 2
+
+// a/b를 소수로 변환하고 소수점 이하 10자리까지 반올림 (`toFixed(10)`)
+// 반올림한 값과 원래 값을 비교
+// 동일하면 유한소수로 간주하여 1 반환
+// 다르면 무한소수로 간주하여 2 반환
