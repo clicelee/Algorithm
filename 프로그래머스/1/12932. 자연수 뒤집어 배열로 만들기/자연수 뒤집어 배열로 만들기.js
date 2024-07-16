e17b00953508c9769a1164be65a3b25dc0ca24cc
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-16 13:31:50 +0900

    몫, 나머지를 이용해 일의 자리 순서 대로  배열 반환: Math.floor do-while

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,1 +1,14 @@
 const solution=(n)=>[...n.toString()].reverse().map(Number);
+
+/* best solution */
+function solution(n) {
+    let arr = []; // arr 배열 초기화
+    do {
+        arr.push(n % 10); 
+        // n의 마지막 자릿수를 arr에 추가 1234 -> 4
+        n = Math.floor(n / 10); 
+        // n의 마지막 자릿수를 제거 n/10: 123.4
+        //Math.floor(): 123
+    } while (n > 0);
+    return arr;
+]
