c834028f1127885191059277c64ee6f562d2c744
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-19 14:05:41 +0900

    better solution using indexOf

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -3,4 +3,6 @@ const solution = (A, B) => {
         if((A.slice(-i)+A.slice(0,-i)) === B) return i;
     return -1;
 }
+/*  better solution */
 // https://www.youtube.com/watch?v=HwpF73JJC8s&list=PLkfUwwo13dlWZxOdbvMhkzhAowaiEjuGS 생각의 전환이 필요한 부분
+let solution=(a,b)=>(b+b).indexOf(a)
