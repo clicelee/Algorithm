76305fb01ea9d99d5d5ff56bdbde0ba405b9f9fd
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-19 12:33:26 +0900

    문자열 합치는 방법으로 풀기

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -3,3 +3,4 @@ const solution = (A, B) => {
         if((A.slice(-i)+A.slice(0,-i)) === B) return i;
     return -1;
 }
+// https://www.youtube.com/watch?v=HwpF73JJC8s&list=PLkfUwwo13dlWZxOdbvMhkzhAowaiEjuGS 생각의 전환이 필요한 부분
