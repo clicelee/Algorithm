9860e7469e7dc2b3037392b60c60cfce3ad179db
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-24 23:09:54 +0900

    삼항연산자 안 쓰고 더 가독성 보기 좋게 바꾸기

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,1 +1,3 @@
 const solution=(x)=> x%(String(x).split('').map(Number).reduce((a,b)=>a+b,0))===0?true:false
+//삼항 연산자 안 쓰는 법... other solution by clice
+const solution=(x)=> x%(String(x).split('').map(Number).reduce((a,b)=>a+b,0))===0
