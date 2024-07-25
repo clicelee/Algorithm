501e491bb57c08b2842198cc3f3f8fee4a5d3bde
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-25 15:29:06 +0900

    [level 0] Title: 이진수 더하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -0,0 +1,27 @@
+const solution=(bin1, bin2) => {
+    return decimalToBinary(binaryToDecimal(bin1) + binaryToDecimal(bin2));
+}
+// 이진수를 십진수로 변환하는 함수
+const binaryToDecimal = (binary) => {
+    let decimal = 0;
+    let length = binary.length;
+    for (let i = 0; i < length; i++) {
+        if (binary[length - 1 - i] === '1') {
+            decimal += Math.pow(2, i);
+        }
+    }
+    return decimal;
+}
+
+// 십진수를 이진수로 변환하는 함수
+const decimalToBinary = (decimal) => {
+    if (decimal === 0) return '0';
+    let binary = '';
+    while (decimal > 0) {
+        binary = (decimal % 2) + binary;
+        decimal = Math.floor(decimal / 2);
+    }
+    return binary;
+}
+
+
