509b193ca0854cb2bd02f4c59b94f4548d9313b5
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-26 23:54:08 +0900

    문자열 숫자를 진짜 숫자로 바꾸는 방법, 나누기 1

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.js
@@ -1,1 +1,8 @@
 const solution = (s) => Number(s);
+
+/* genius solution */
+function strToInt(str){
+  return str/1
+  }
+JavaScript에서는 산술 연산자가 피연산자 중 하나가 숫자가 아닌 경우, 이를 숫자로 변환하려고 시도한다. 
+따라서 어떤 값을 / 연산자를 사용하여  연산하려고 하면 강제로 숫자로 변환해준다
