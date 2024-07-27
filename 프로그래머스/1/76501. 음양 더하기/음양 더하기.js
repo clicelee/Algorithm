4fe1d610776ae91d433d68980ced2d9b4bb89e3b
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-07-27 23:21:59 +0900

    고차함수 reduce

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -4,3 +4,6 @@ function solution(absolutes, signs) {
         (signs[i]===true)?(answer+=absolutes[i]):(answer-=absolutes[i]);
     return answer;
 }
+/* better solution */
+//고차함수 reduce 사용하기
+const solution =(absolutes, signs) => absolutes.reduce((sum, num, i) => sum + num * (signs[i] ? 1 : -1), 0);
