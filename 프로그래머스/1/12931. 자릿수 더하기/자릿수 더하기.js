b87043482a52dbbda3d72a81adbbc807c87ab906
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-01 22:53:11 +0900

    중간 풀이 추가

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,1 +1,2 @@
-const solution =(n) => [...(n+'')].reduce((a,b)=>a+Number(b),0);
+const solution =(n) => [...(n+'')].reduce((a,b)=>a+b,0); //-> 이러면 1,2,3 더했을때 0123이렇게 나온다. 
+const solution =(n) => [...(n+'')].reduce((a,b)=>a+Number(b),0); //a가 0이니까 그 다음 더할 b 부터 Number을 해준다
