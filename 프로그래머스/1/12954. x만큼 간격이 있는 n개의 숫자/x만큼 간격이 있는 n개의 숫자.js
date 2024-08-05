4ea995636f3d671289a00c78a7327e9a5b635cc8
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-05 15:15:39 +0900

    이전 코드 (개선 전)

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,1 +1,6 @@
 const solution = (x, n) => Array(n).fill(x).map((e,i)=>e+i*x)
+//previous code
+// const solution = (x,n) =>{
+//     let answer=Array(n).fill(x);
+//     return answer.map((e,i)=>e+i*x);
+// }
