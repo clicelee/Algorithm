9d0dcb55d9703dc50e57ddfba96f420a36bf9595
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-09 13:51:07 +0900

    reduce를 이용한 better solution

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,1 +1,12 @@
 const solution = (a, b) => Array.from({ length:a.length }, (_, index) => index).reduce((arr,cur)=> arr+a[cur]*b[cur],0)
+
+/* 풀이 */
+const solution = (a, b) => {
+    let array = Array.from({ length:a.length }, (_, index) => index);
+    return array.reduce((arr,cur)=> arr+a[cur]*b[cur],0)
+}
+
+/* better solution */
+function solution(a, b) {
+    return a.reduce((acc, _, i) => acc += a[i] * b[i], 0);
+}
