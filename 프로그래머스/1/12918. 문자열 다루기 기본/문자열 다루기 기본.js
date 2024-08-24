c3f60591cb05da041971e46ae5fd4a458968bff1
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-25 00:05:47 +0900

    Update 문자열 다루기 기본.js

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,1 +1,8 @@
 const solution = (s) => (s.length === 4 || s.length === 6) && [...s].every(c => !isNaN(c))
+/*better solution */
+//정규표현식 이용
+// function solution(s){
+//     var regex = /^\d{6}$|^\d{4}$/;
+
+//   return regex.test(s);
+// }
