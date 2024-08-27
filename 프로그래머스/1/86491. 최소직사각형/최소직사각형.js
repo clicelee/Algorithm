57855e3b99593aa01262efcea5471437d58952d9
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-27 23:59:05 +0900

    add better solution

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -18,3 +18,10 @@ const solution=(sizes)=> {
     // 최종 지갑 크기
     return maxWidth * maxHeight;
 }
+
+
+/* better solution */
+function solution(sizes) {
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
+}
