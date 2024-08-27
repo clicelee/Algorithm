4c5c4d0f5352ec712d06e751113afe42dd4aed24
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-08-27 23:33:58 +0900

    [level 1] Title: 최소직사각형, Time: 3.66 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,20 @@
+const solution=(sizes)=> {
+    const findMax = (array, index) => {
+        let max = array[0][index];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i][index] > max) { 
+                max = array[i][index];
+            }
+        }
+        return max;
+    };
+    // 각 사이즈 배열에서 첫 번째 요소가 더 크면 위치를 바꾸는 로직
+    let adjustedSizes = sizes.map(size => {
+        return size[0] > size[1] ? [size[1], size[0]] : size;
+    });
+    // 가로와 세로의 최대값을 찾아서 출력
+    let maxWidth = findMax(adjustedSizes, 0);
+    let maxHeight = findMax(adjustedSizes, 1);
+    // 최종 지갑 크기
+    return maxWidth * maxHeight;
+}
