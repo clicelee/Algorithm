df1ee2bc2351c4412821a12c56d1fc3e8098af72
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-09 18:36:51 +0900

    [level 2] Title: 올바른 괄호, Time: 10.07 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,13 +1,8 @@
-const solution = (s) => {
-    let answer = [];
-    
-    for(i of [...s]){
-        if(i==="(")
-            answer.push(i);
-        else{
-            if(answer.length===0) return false;
-            answer.pop(i);
-        }
-    }
-    return (answer.length===0);
+function solution(s){
+    var answer = true;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+
+    return answer;
 }
