acfbe0560874a5caaf72d05675316c80685c1292
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-10 18:40:03 +0900

    add better solution

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -9,3 +9,6 @@ const solution = (give, receive, current)=>{
 }
 
 //잘못된 테스트 케이스를 사용해서 계속 혼자서 오류가 났었다. 이상한 부분을 못 찾아서 일단 제출해봤더니 통과! 앞으로 시간낭비 하지 않도록 테스트 케이스를 만들때 검토하자.
+/* another solution */
+solution = (a, b, n) => Math.floor(Math.max(n - b, 0) / (a - b)) * b
+//수학을 열심히 공부하자
