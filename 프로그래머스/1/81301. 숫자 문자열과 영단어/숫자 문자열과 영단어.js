db87c99bc1757243ad10e910a6e2247f5d3ccc72
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-10 10:56:30 +0900

    index i 의 특징 활용 보충 설명

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -29,7 +29,12 @@ const solution = (s)=>{
 }
 
 /*better solution */
-//인덱스와 영어 문자를 대응시키고 인덱스를 배열에 넣는 방식
+// //인덱스와 영어 문자를 대응시키고 인덱스를 배열에 넣는 방식
+// 코드에서 i를 넣는 이유는, numbers[i]에 해당하는 단어가 나오면 그걸 숫자로 바꾸기 위해서이다. i는 zero부터 nine까지의 단어에 대응하는 숫자를 의미한다.
+
+// 즉, split으로 단어를 기준으로 나누고, join(i)로 그 단어 대신 숫자 i로 이어주는 것이다. 그래서 "one"은 1로, "two"는 2로 바꾸는 것이다.
+
+// 쉽게 말해, i는 그 단어가 몇 번째 숫자인지를 나타내서, 그걸 숫자로 치환하는 역할을 하는 것이다.
 function solution(s) {
     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     var answer = s;
