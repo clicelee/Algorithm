9e37c0fd634de4b7d15024b5c4196dcf1087072e
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-15 23:40:05 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 1.26 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -5,6 +5,5 @@ function solution(n, m) {
             if((n%i==0)&&(m%i==0)) answer=i;
         return answer;
     }
-    const LCM=(n,m)=> ((n/GCD(n,m))*(m/GCD(n,m))*GCD(n,m));
-    return [GCD(n,m), LCM(n,m)];
+    return [GCD(n,m), (n*m/GCD(n,m))];
 }
