f6b14e5827b34982bdd922932ae1e500f6fa20f0
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-15 23:58:31 +0900

    best solution 최대공약수와 최소공배수.js

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -7,3 +7,10 @@ function solution(n, m) {
     }
     return [GCD(n,m), (n*m/GCD(n,m))];
 }
+
+/* best solution */
+function gcdlcm(a, b) {
+    var r;
+    for(var ab= a*b;r = a % b;a = b, b = r){}
+    return [b, ab/b];
+}
