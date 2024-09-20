42b6edbf0e077a12a49794aa4782265ca6dc4ad3
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-20 14:15:08 +0900

    add how i solved(how it works)

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,1 +1,8 @@
 const solution = (n) => parseInt(n.toString(3).split('').reverse().join(''),3);
+/* how it works */
+// function solution(n) {
+//     let tetra = n.toString(3);
+//     let reverseTetra =tetra.split('').reverse().join('');
+//     let answer=parseInt(reverseTetra,3);
+//     return answer;
+// }
