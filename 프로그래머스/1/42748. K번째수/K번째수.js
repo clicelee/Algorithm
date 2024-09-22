0c117ee896698ae37b2e59ee7561ac04d71a5124
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-22 22:26:21 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,1 @@
-function solution(array, commands) {
-    var answer = [];
-    for(let i=0; i<commands.length; i++){
-        let slicedArray=array.slice(commands[i][0]-1,commands[i][1]);
-        answer.push((slicedArray.sort((a,b)=>a-b))[commands[i][2]-1]);
-    }
-    return answer;
-}
+const solution = (array, commands) => commands.map(i=>array.slice(i[0]-1,i[1]).sort((a,b)=>a-b)[i[2]-1]);
