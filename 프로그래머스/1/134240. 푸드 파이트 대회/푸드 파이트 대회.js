c65a47545507303e1294e5b950fceafdbc7efe1b
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-23 11:41:13 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,1 +1,9 @@
 const solution= (food) => String([...food.map(i=>(i%2!=0)?(i-1)/2:(i/2)).map((e,i)=>(e!=0)?String(i).repeat(e):''),0,...food.map(i=>(i%2!=0)?(i-1)/2:(i/2)).map((e,i)=>(e!=0)?String(i).repeat(e):'').reverse()].join(''));
+// function solution(food) {
+//     let evenFood=food.map(i=>{return (i%2!=0)?(i-1)/2:(i/2)});
+//     console.log(evenFood);
+//     let answer=evenFood.map((e,i)=>{
+//         return (e!=0)?String(i).repeat(e):'';
+//     });  
+//     return String([...answer,0,...answer.reverse()].join(''));
+// }
