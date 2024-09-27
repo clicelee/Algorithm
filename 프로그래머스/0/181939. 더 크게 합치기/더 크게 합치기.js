6ebc7c8bac116708ddb2ddbd4aed0708f1947c75
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-27 12:37:05 +0900

    better solution using Math.max and template literal

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,1 +1,10 @@
 const solution=(a, b) => Math.max(Number([...(a+''+b)].join('')),Number([...(b+''+a)].join('')))
+
+/*기존 코드*/
+// const solution=(a, b) => Number([...(a+''+b)].join(''))>Number([...(b+''+a)].join(''))?(Number([...(a+''+b)].join(''))):Number([...(b+''+a)].join(''));
+
+/* better solution */
+//Math.max와 템플릿 리터럴을 이용하면 된다.
+function solution(a, b) {
+    return Math.max(Number(${a}${b}), Number(${b}${a}))
+}
