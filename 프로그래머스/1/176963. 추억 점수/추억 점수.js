15860fff999cb08407ca8e9bf877352762430374
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-09-29 18:25:06 +0900

    another solution using indexOf

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -4,3 +4,8 @@ const solution=(name, yearning, photo)=> {
         return acc;
     },{});
     return photo.map((e,i) => e.reduce((acc,person)=>acc+(dictionary[person]||0),0));}
+/* another solution */
+//using indexOf
+function solution(name, yearning, photo) {
+    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
+}
