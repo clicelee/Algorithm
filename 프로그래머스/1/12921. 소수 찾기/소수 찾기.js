60e0cd63360b71bd2a3fa0c08824db9aad81aa3b
𝒄𝒍 𝒊 𝒄 𝒆 <131771046+clicelee@users.noreply.github.com>
2024-11-08 12:16:55 +0900

    Watch out for runtime errors!

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,2 +1,18 @@
 const solution=(n)=>{ const isPrime = Array(n + 1).fill(true); for(let i=2; i<=Math.sqrt(n); i++) if(isPrime[i]) for (let j= i*i; j<=n; j+=i) isPrime[j]=false; return isPrime.filter(val => val).length-2;
 };
+
+//기존에 runtime 에러 났던 코드. n의 최댓값이 1000000인데, for 문을 두번 돌려야 해서 runtime error가 났다
+
+// function solution(n) {
+//     let count=0;
+//     for(let k=2; k<=n; k++)
+//         if(isPrimeNumber(k)===1) count++;
+//     return count;
+// }
+
+// function isPrimeNumber(m){
+//     for(let i=2; i<=Math.sqrt(m); i++)
+//         if(m%i===0) return 0  //소수가 아니면 0 반환
+//     return 1; //소수이면 1 반환
+// }
+// runtime error
